c1f061b096780db15a87ba8bbfea8f3a645cb26b
Atul Kumar <atulsoft@gmail.com>
2024-08-09 21:53:41 +0530

    did nothing

--- imageDetection/category.py
+++ imageDetection/category.py
@@ -3,6 +3,7 @@ import shutil
 import re
 
 import numpy as np
+
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
