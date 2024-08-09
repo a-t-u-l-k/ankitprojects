bac4c5dfde3ae4fe8b0c5ecd0743980658ca76e4
Atul Kumar <atul.1.kumar@oracle.com>
2024-08-09 21:50:05 +0530

    changes

--- training.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
-import numpy as np
-import matplotlib.pyplot as plt
